0f2129a385dad36f2d340a482405beaaaf158ece
hashmib <hashmi.sbilal@gmail.com>
2020-05-31 21:37:58 -0400

    Refactored and built codebase from ground up. Look at me, i have fancy commit messages

--- processing.py
+++ processing.py
@@ -2,104 +2,86 @@ import pandas as pd
 import re
 
 # Raw text from archive:
-# [4/29/19, 12:25:49 PM] Bilal Hashmi: Yes exactly
+# [4/29/19, 1:03:21 PM] Talal Siddiqui: What's mine
+
 
 # Goal is to break down to raw text into four tokens
 # {Date}, {Time} - {Author}: {Message}
 
-def isDateAndTime(raw_text):
-    sample = "[4/29/19, 12:25:49 PM] Bilal Hashmi: Yes exactly"
-    # Learn how REGEX works: https://medium.com/tech-tajawal/regular-expressions-the-last-guide-6800283ac034
-    pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
 
-    result = re.match(pattern, sample)
-    if result:
-        print("WHAY")
-        return True
-    print("PAKISTAN WHAT")
-    return False
+# todo: primitive implementation, needs regex for proper handling
+def isDateTime(raw_text):
+    return "[" in raw_text and "]" in raw_text
 
 
 
-def isAuthor(text):
-    patterns = [
-        '([\w]+):',                        # First Name
-        '([\w]+[\s]+[\w]+):',              # First Name + Last Name
-        '([\w]+[\s]+[\w]+[\s]+[\w]+):',    # First Name + Middle Name + Last Name
-        '([+]\d{2} \d{5} \d{5}):',         # Mobile Number (Pakistan)
-        '([+]\d{2} \d{3} \d{3} \d{4}):',   # Mobile Number (US)
-        '([+]\d{2} \d{4} \d{7})'           # Mobile Number (UK/Europe)
-    ]
-    pattern = '^' + '|'.join(patterns)
-    result = re.match(pattern, text)
-    if result:
-        return True
-    return False
-
-
-def parseData(line_in):    
-    split = line_in.split(" - ")
-    dateTime = split[0]
+def parseData(line_in):
+    #line_in = "‎[5/31/20, 19:20:23] ‪+92 321 2287108‬: ‎image omitted" 
+    
+    split_str = line_in.split("] ")
+    dateTime = split_str[0].split("[")[1]
 
     date, time = dateTime.split(", ")
+    remainder = ' '.join(split_str[1:])
 
-    #todo 
-    message = ' '.join(split[1:])
+    checkAuthorChar = ':'
 
-    if isAuthor(message):
-        splitMessage = message.split(': ')
-        author = splitMessage[0] 
-        message = ' '.join(splitMessage[1:])
-    
+    # Messages without an author (:) are ignored (eg - [4/29/19, 12:06:20] ‎Adeel Khalique created this group)
+    if checkAuthorChar in remainder:
+        split_remainder = remainder.split(': ')
+        author = split_remainder[0] 
+        message = ' '.join(split_remainder[1:])
+
     # Author will return null for all raw text similar to "Bilal Hashmi created the group"
     else:
         author = None
+        message = None
     return date, time, author, message
+
 
 def main():
     processedData = []
     path = '_chat.txt'
 
-    line = "[4/29/19, 12:25:49 PM] Bilal Hashmi: Yes exactly"
-    data = parseData(line)
-    for elt in data:
-        print(elt)
-    # with open(path, encoding="utf-8") as file:
-    #     # Ignore first line of the conversation ("...Messages to this group are now secured with end-to-end encryption.")
-    #     file.readline()
+    with open(path, encoding="utf-8") as file:
+        # Ignore first line of the conversation ("...Messages to this group are now secured with end-to-end encryption.")
+        file.readline()
 
-    #     # For multi-line messages
-    #     buffer = []
-    #     date, time, author = None, None, None 
+        # For multi-line messages
+        buffer = ""
+        date, time, author = None, None, None
         
-    #     while True:
-    #         line = file.readline()
+        while True:
+            line = file.readline()
             
-    #         # EOF reached
-    #         if not line:
-    #             break
+            # EOF reached
+            if not line:
+                break
             
-    #         # Remove random leading and trailing whitespaces
-    #         line = line.strip()
+            # Remove random leading and trailing whitespaces
+            line = line.strip()
 
-    #         # New message
-    #         if isDateAndTime(line):
-    #             print("in here mate")
-    #             # If buffer full from previous iteration, append
-    #             if len(buffer) > 0:
-    #                 processedData.append([date, time, author, ' '.join(buffer)])
+            # New message
+            if isDateTime(line):
+                # If buffer has data from previous iteration, append
+                if len(buffer) > 0:
+                    processedData.append([date, time, author, buffer])
 
-    #             # Clear the buffer
-    #             buffer.clear()
+                # Clear the buffer
+                buffer = ""
 
-    #             date, time, author, message = parseData(line)
-    #             buffer.append(message)
-    #         else:
-    #             # At this point, if its not date And Time, its part of a multi-line message, so append to message buffer
-    #             buffer.append(line)
+                date, time, author, message = parseData(line)
+                if message:
+                    buffer += message
+            else:
+                # At this point, if its not date time, its part of a multi-line message, so append to message buffer
+                buffer += line
+
+        processedData.append([date, time, author, buffer])    
     
-
-    # dataframe = pd.DataFrame(processedData, columns=['Date', 'Time', 'Author', 'Message'])
+    dataframe = pd.DataFrame(processedData, columns=['Date', 'Time', 'Author', 'Message'])
+    return dataframe
+    
     
 
 
--- sentimentan.py
+++ sentimentan.py
@@ -1,9 +1,14 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 from processing import main as processed
 
 def main():
-    print("Hello world")
+    df = processed()    
+    author_value_counts = df['Author'].value_counts() # Number of messages per author
+    top_10_author_value_counts = author_value_counts.head(10) # Number of messages per author for the top 10 most active authors
+    top_10_author_value_counts.plot.barh() # Plot a bar chart using pandas built-in plotting apis
 
+    plt.show(block=True)
 
 if __name__ == "__main__":
     main()
